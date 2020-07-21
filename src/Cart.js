4932b147d75c5d22d93c2dc6d2a68232d48171a9
siddy1998 <siddhantrathoreinfinity@gmail.com>
2020-07-21 17:24:04 +0530

    handle inc and dec qty in Cart.js

--- src/Cart.js
+++ src/Cart.js
@@ -32,6 +32,39 @@ class Cart extends React.Component {
       
     }
 
+    handleIncreaseQuantity = (product) =>{
+       // console.log('Hey pls inc qty of the product');
+        
+        const {products} = this.state;
+        const index = products.indexOf(product);
+        
+        products[index].qty += 1;
+
+        this.setState({
+            products   //or write products : products
+        })
+
+
+    }
+
+    handleDecreaseQuantity = (product) => {
+       // console.log('Hey pls dec qty of the product');
+
+        const { products } = this.state;
+        const index = products.indexOf(product);
+
+        if(products[index].qty === 0)
+            return;
+
+        products[index].qty -= 1;
+
+        this.setState({
+            products //or write products : products
+        })
+
+
+    }
+
     render() {
         const {products} = this.state;
         return(
@@ -40,6 +73,8 @@ class Cart extends React.Component {
                     return <CartItem 
                             product={product}
                             key = {product.id}
+                            onIncreaseQuantity = {this.handleIncreaseQuantity}
+                            onDecreaseQuantity = {this.handleDecreaseQuantity}
                             />
                 })}
             </div>
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,76 +1,10 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-    // constructor() {
-    //     super();
-    //     this.state = {
-    //         price: 999,
-    //         title: 'Mobile Phone',
-    //         qty: 1,
-    //         img: ''
-    //     }
-    //     // this.increaseQuantity = this.increaseQuantity.bind(this);
-    //      // this.testing();
-    // }
-
-    // testing () {
-  //   const promise = new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       resolve('done');
-  //     }, 5000);
-  //   })
-
-  //   promise.then(() => {
-  //     // setState acts like a synchronus call
-  //     this.setState({ qty: this.state.qty + 10 });
-
-  //     this.setState({ qty: this.state.qty + 10 });
-
-  //     this.setState({ qty: this.state.qty + 10 });
-
-  //     console.log('state', this.state);
-  //   });
-  // }
-
-    increaseQuantity = () => {
-           // this.state.qty +=1;
-           // console.log('this', this.state);
-        
-        //first way of setState
-        //    this.setState({
-        //        qty : this.state.qty + 1
-        //    });
-        //second way of setState
-        // setState form 2 - if prevState required use this
-           this.setState((prevState) =>{
-               return {
-                   qty : prevState.qty + 1
-               }
-           });
-        
-    }
-    
-    decreaseQuantity = () => {
-       
-        const {qty} = this.state;
-        //dont dec if quantity is already 0
-        if(qty===0)
-        {
-            return;
-        }
-
-        this.setState((prevState) => {
-            return {
-                qty: prevState.qty - 1
-            }
-        });
-
-    }
-    
-
-    render() {
+     render() {
         console.log('this.props', this.props);
         const { price,title,qty } = this.props.product;
+        const { product,onIncreaseQuantity,onDecreaseQuantity} = this.props;
         return (
             <div className = "cart-item">
                 {/* {this.props.jsx } */}
@@ -88,13 +22,13 @@ class CartItem extends React.Component {
                         alt="increase" 
                         className="action-icons" 
                         src="https://image.flaticon.com/icons/svg/992/992651.svg"
-                        onClick = {this.increaseQuantity}
+                        onClick = {() => onIncreaseQuantity(product)}
                     />
                     <img 
                         alt="decrease" 
                         className="action-icons" 
                         src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
-                        onClick={this.decreaseQuantity}
+                        onClick={() => onDecreaseQuantity(product)}
                     />
                     <img 
                         alt="delete" 
